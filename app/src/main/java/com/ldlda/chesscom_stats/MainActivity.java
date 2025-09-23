8ff4e41d7a18dc88c6b561183cb824f354014871
BSFactor <2005nguyennamkhanh@gmail.com>
2025-09-23 22:41:19 +0700

    Menu toolbar, re-order the layout, background song

--- app/src/main/java/com/ldlda/chesscom_stats/MainActivity.java
+++ app/src/main/java/com/ldlda/chesscom_stats/MainActivity.java
@@ -1,6 +1,14 @@
 package com.ldlda.chesscom_stats;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +21,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import com.ldlda.chesscom_stats.adapter.ScreenSlidePagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    //Available tabs
-
+    private MediaPlayer bg_song;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +33,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        //private final String[] tabTitles = {"Home", "Favorites", "Hall of Fame", "Lessons"};
+        //Availabe tabs
         final String[] tabTitles = {
                 getString(R.string.home),
                 getString(R.string.fav),
                 getString(R.string.hall_fame),
                 getString(R.string.lesson)
         };
+
         ViewPager2 viewPager = findViewById(R.id.view_pager);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         viewPager.setAdapter(new ScreenSlidePagerAdapter(this));
@@ -40,5 +48,31 @@ public class MainActivity extends AppCompatActivity {
         new TabLayoutMediator(tabLayout, viewPager, (tab, position) ->
                 tab.setText(tabTitles[position])
         ).attach();
+
+        // Adding the menu toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Music
+        bg_song = MediaPlayer.create(this,R.raw.open_sky);
+        bg_song.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        // For some reason switch giving an error of constant expression required, so beta virgin
+        if (id == R.id.menu_about_us) {
+            Toast.makeText(this, "Group 14 on topic 25: chess.com stats", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
