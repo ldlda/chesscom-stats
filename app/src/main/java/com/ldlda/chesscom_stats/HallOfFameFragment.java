09844ad3323cb3264a1577e49394737368db686c
lda <lda@ldlda.com>
2025-09-26 14:39:34 +0700

    Migrate to androidx SearchView

--- app/src/main/java/com/ldlda/chesscom_stats/HallOfFameFragment.java
+++ app/src/main/java/com/ldlda/chesscom_stats/HallOfFameFragment.java
@@ -5,10 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
