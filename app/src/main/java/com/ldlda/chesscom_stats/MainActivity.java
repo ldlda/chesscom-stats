29a7aee9380262936ce3b5de29d0bd909889a6aa
lda <lda@ldlda.com>
2025-09-26 11:39:10 +0700

    Apply suggestion; fix sometimes crash by switching language
    Added selectedItemId state saved/restored via onSaveInstanceState.
    Introduced ensureFragments() to find existing fragments by tag or create them once, then commitNow.
    Introduced showSelected(menuId) to hide all and show the target, setting currentFragment reliably.
    Replaced inline setup with calls to ensureFragments() and showSelected(selectedItemId).
    Updated the nav listener to use fragmentFor(menuId) and keep selectedItemId in sync.

--- app/src/main/java/com/ldlda/chesscom_stats/MainActivity.java
+++ app/src/main/java/com/ldlda/chesscom_stats/MainActivity.java
@@ -2,33 +2,35 @@ package com.ldlda.chesscom_stats;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ldlda.chesscom_stats.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+
+    private static final String KEY_SELECTED = "selectedItemId";
+    private static final String TAG_HOME = "tab:home";
+    private static final String TAG_HALL = "tab:hall";
+    private static final String TAG_FAV = "tab:fav";
+    private static final String TAG_LESSONS = "tab:lessons";
     private MediaPlayer backgroundSong;
 
     // Cached fragment references for show/hide pattern
-    private Fragment homeFragment;
-    private Fragment hallFragment;
-    private Fragment favoritesFragment;
-    private Fragment lessonsFragment;
-    private Fragment currentFragment;
+    private Fragment homeFragment, hallFragment, favoritesFragment, lessonsFragment, currentFragment;
+    private int selectedItemId = R.id.home;
 
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,69 +44,119 @@ public class MainActivity extends AppCompatActivity {
         // Bottom navigation handling using show/hide (create fragments once)
         BottomNavigationView bottomNavigationView = binding.bottomNavigation;
 
-        if (savedInstanceState == null) {
-            homeFragment = new HomeFragment();
-            hallFragment = new HallOfFameFragment();
-            favoritesFragment = new FavoritesFragment();
-            lessonsFragment = new LessonsFragment();
-            currentFragment = homeFragment;
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, homeFragment, "home")
-                    .add(R.id.fragment_container, hallFragment, "hall").hide(hallFragment)
-                    .add(R.id.fragment_container, favoritesFragment, "favorites").hide(favoritesFragment)
-                    .add(R.id.fragment_container, lessonsFragment, "lessons").hide(lessonsFragment)
-                    .commit();
-        } else {
-            homeFragment = getSupportFragmentManager().findFragmentByTag("home");
-            hallFragment = getSupportFragmentManager().findFragmentByTag("hall");
-            favoritesFragment = getSupportFragmentManager().findFragmentByTag("favorites");
-            lessonsFragment = getSupportFragmentManager().findFragmentByTag("lessons");
-            if (homeFragment != null && homeFragment.isVisible()) currentFragment = homeFragment;
-            else if (hallFragment != null && hallFragment.isVisible()) currentFragment = hallFragment;
-            else if (favoritesFragment != null && favoritesFragment.isVisible()) currentFragment = favoritesFragment;
-            else if (lessonsFragment != null && lessonsFragment.isVisible()) currentFragment = lessonsFragment;
+        if (savedInstanceState != null) {
+            selectedItemId = savedInstanceState.getInt(KEY_SELECTED, R.id.home);
         }
+
+        ensureFragments();
+        showSelected(selectedItemId);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int menuId = item.getItemId();
-            Fragment target;
-            if (menuId == R.id.leaderboards) {
-                target = hallFragment;
-            } else if (menuId == R.id.favorites) {
-                target = favoritesFragment;
-            } else if (menuId == R.id.lessons) {
-                target = lessonsFragment;
-            } else {
-                target = homeFragment;
-            }
+            Fragment target = fragmentFor(menuId);
             if (target != null && target != currentFragment) {
-                getSupportFragmentManager().beginTransaction()
+                getSupportFragmentManager()
+                        .beginTransaction()
                         .hide(currentFragment)
                         .show(target)
                         .commit();
                 currentFragment = target;
+                selectedItemId = menuId;
             }
             return true;
         });
 
-        if (savedInstanceState == null) {
-            bottomNavigationView.setSelectedItemId(R.id.home);
+        // Keep BottomNavigationView selection in sync
+        if (bottomNavigationView.getSelectedItemId() != selectedItemId) {
+            bottomNavigationView.setSelectedItemId(selectedItemId);
         }
 
         // Music
         backgroundSong = MediaPlayer.create(this, R.raw.open_sky);
         backgroundSong.setLooping(true);
-        backgroundSong.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: triggered");
+        if (backgroundSong != null && !backgroundSong.isPlaying()) {
+            backgroundSong.start();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // Only release the song when you quit out the app completely
         if (backgroundSong != null) {
             backgroundSong.release();
             backgroundSong = null;
         }
-        binding = null;
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (backgroundSong != null && backgroundSong.isPlaying()) {
+            backgroundSong.pause();
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_SELECTED, selectedItemId);
+    }
+
+    private void ensureFragments() {
+        var fm = getSupportFragmentManager();
+        var tx = fm.beginTransaction();
+
+        homeFragment = fm.findFragmentByTag(TAG_HOME);
+        hallFragment = fm.findFragmentByTag(TAG_HALL);
+        favoritesFragment = fm.findFragmentByTag(TAG_FAV);
+        lessonsFragment = fm.findFragmentByTag(TAG_LESSONS);
+
+        if (homeFragment == null) {
+            homeFragment = new HomeFragment();
+            tx.add(R.id.fragment_container, homeFragment, TAG_HOME).hide(homeFragment);
+        }
+        if (hallFragment == null) {
+            hallFragment = new HallOfFameFragment();
+            tx.add(R.id.fragment_container, hallFragment, TAG_HALL).hide(hallFragment);
+        }
+        if (favoritesFragment == null) {
+            favoritesFragment = new FavoritesFragment();
+            tx.add(R.id.fragment_container, favoritesFragment, TAG_FAV).hide(favoritesFragment);
+        }
+        if (lessonsFragment == null) {
+            lessonsFragment = new LessonsFragment();
+            tx.add(R.id.fragment_container, lessonsFragment, TAG_LESSONS).hide(lessonsFragment);
+        }
+        tx.commitNow();
+    }
+
+    private void showSelected(int menuId) {
+        Fragment target = fragmentFor(menuId);
+        if (target == null) target = homeFragment;
+        var tx = getSupportFragmentManager().beginTransaction();
+        if (homeFragment != null) tx.hide(homeFragment);
+        if (hallFragment != null) tx.hide(hallFragment);
+        if (favoritesFragment != null) tx.hide(favoritesFragment);
+        if (lessonsFragment != null) tx.hide(lessonsFragment);
+        tx.show(target).commitNow();
+        currentFragment = target;
+    }
+
+    private Fragment fragmentFor(int menuId) {
+        if (menuId == R.id.home) return homeFragment;
+        if (menuId == R.id.leaderboards) return hallFragment;
+        if (menuId == R.id.favorites) return favoritesFragment;
+        if (menuId == R.id.lessons) return lessonsFragment;
+        return null;
     }
 
     @Override
