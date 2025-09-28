0d478283cb5e04bf2a39d67826dbd651baa1e9f5
khanh <khanhnq.23bi14215@usth.edu.vn>
2025-09-28 23:43:44 +0700

    change everything to recyclerview

--- app/src/main/java/com/ldlda/chesscom_stats/LessonsFragment.java
+++ app/src/main/java/com/ldlda/chesscom_stats/LessonsFragment.java
@@ -4,59 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ldlda.chesscom_stats.adapter.LessonAdapter;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LessonsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
     public LessonsFragment() {
         // Required empty public constructor
     }
 
-    public static LessonsFragment newInstance() {
-        return new LessonsFragment();
-    }
-
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_lessons, container, false);
 
-        // Array of lesson ImageView IDs
-        int[] lessonIds = {
-                R.id.introduction,
-                R.id.lesson_one,
-                R.id.lesson_two,
-                R.id.lesson_three,
-                R.id.lesson_four,
-                R.id.lesson_five
-        };
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Loop through each ImageView and set a click listener
-        for (int i = 0; i < lessonIds.length; i++) {
-            final int lessonIndex = i; // capture index
-            ImageView lessonView = view.findViewById(lessonIds[i]);
-            lessonView.setOnClickListener(v -> openLesson(lessonIndex));
-        }
+        List<String> lessonTitles = Arrays.asList(
+                "Lesson 1:",
+                "Lesson 2:",
+                "Lesson 3:",
+                "Lesson 4:",
+                "Lesson 5:"
+        );
+
+        LessonAdapter adapter = new LessonAdapter(lessonTitles, this::openLesson);
+        recyclerView.setAdapter(adapter);
 
         return view;
     }
 
-    /**
-     * Opens a ContentLesson fragment with the given lesson index.
-     */
     private void openLesson(int lessonIndex) {
-        ContentLesson contentLesson = ContentLesson.newInstance(lessonIndex);
+        LessonContents contentLesson = LessonContents.newInstance(lessonIndex);
 
         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.main, contentLesson, "ContentLesson")
+                .replace(R.id.fragment_container, contentLesson, "ContentLesson")
                 .addToBackStack(null)
                 .commit();
     }
