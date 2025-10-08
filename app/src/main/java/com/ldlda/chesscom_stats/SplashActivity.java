86055cd52c59d9d34010e893ece8cc39087b783c
BSFactor <2005nguyennamkhanh@gmail.com>
2025-10-08 14:38:12 +0700

    Complete splash screen

--- app/src/main/java/com/ldlda/chesscom_stats/SplashActivity.java
+++ app/src/main/java/com/ldlda/chesscom_stats/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.ldlda.chesscom_stats;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash_motion), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 2500);
     }
 }
