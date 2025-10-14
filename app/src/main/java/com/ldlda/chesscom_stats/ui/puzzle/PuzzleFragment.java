1aad154d6394f95a6c6e2457922b2ce20baee570
BSFactor <2005nguyennamkhanh@gmail.com>
2025-10-15 00:24:35 +0700

    Puzzle solve webview

--- app/src/main/java/com/ldlda/chesscom_stats/ui/puzzle/PuzzleFragment.java
+++ app/src/main/java/com/ldlda/chesscom_stats/ui/puzzle/PuzzleFragment.java
@@ -1,6 +1,7 @@
 package com.ldlda.chesscom_stats.ui.puzzle;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,10 +25,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class PuzzleFragment extends Fragment {
+    private static final String TAG = "PuzzleFragment";
     private ProgressBar progressBar;
     private MaterialButton solveURI;
     private ImageView puzzleView;
     private TextView puzzleName;
+
+    private String puzzleUrl;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -50,20 +55,50 @@ public class PuzzleFragment extends Fragment {
                 if (response.isSuccessful() && response.body() != null) {
                     PuzzleData puzzle = response.body();
                     puzzleName.setText(puzzle.title);
+                    puzzleUrl = puzzle.url;
+
                     Glide.with(requireContext())
                             .load(puzzle.image)
                             .into(puzzleView);
                 }else{
-                    Toast.makeText(requireContext(),"Error getting puzzle-Response code:"+ response.code(),Toast.LENGTH_SHORT).show();
+                    Log.e(TAG,"Puzzle load error-Response code:"+response.code());
+                    Toast.makeText(requireContext(),"Error loading puzzle-Response code:"+ response.code(),Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<PuzzleData> call, Throwable t) {
                 progressBar.setVisibility(View.GONE);
+                Log.e(TAG,t.getMessage());
                 Toast.makeText(requireContext(),"Error getting puzzle: "+ t.getMessage(),Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        solveURI.setOnClickListener(v -> {
+            if (puzzleUrl != null) {
+                Bundle bundle = new Bundle();
+                bundle.putString("puzzle_url", puzzleUrl);
+
+                PuzzleSolve webFragment = new PuzzleSolve();
+                webFragment.setArguments(bundle);
+
+                requireActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(
+                                R.anim.slide_in_right,  // animation for the new fragment entering
+                                R.anim.slide_out_left,      // animation for the current fragment exiting
+                                R.anim.slide_in_left,   // animation when coming back (pop enter)
+                                R.anim.slide_out_right      // animation when going back (pop exit)
+                        )
+                        .replace(R.id.fragment_container, webFragment)
+                        .addToBackStack(null)
+                        .commit();
+            } else {
+                Toast.makeText(requireContext(), "Puzzle not loaded yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         return view;
     }
