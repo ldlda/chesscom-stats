3bcd7e3446335a64b3e2a218300a5143f364adef
lda <lda@ldlda.com>
2025-10-14 19:06:23 +0700

    Finish merge
    Signed-off-by: lda <lda@ldlda.com>

--- app/src/main/java/com/ldlda/chesscom_stats/ui/home/HomeFragment.java
+++ app/src/main/java/com/ldlda/chesscom_stats/ui/home/HomeFragment.java
@@ -1,20 +1,24 @@
 package com.ldlda.chesscom_stats.ui.home;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.ldlda.chesscom_stats.R;
 import com.ldlda.chesscom_stats.ui.ai.FidePredictFragment;
 import com.ldlda.chesscom_stats.ui.clubs.ClubFragment;
 import com.ldlda.chesscom_stats.ui.playersearch.PlayerSearchFragment;
+import com.ldlda.chesscom_stats.ui.puzzle.PuzzleFragment;
 
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
     //Inflate fragment
@@ -22,7 +26,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        Button plr_search_btn = view.findViewById(R.id.plr_searcher);
+        CardView plr_search_btn = view.findViewById(R.id.plr_searcher);
 
         plr_search_btn.setOnClickListener(v -> {
             requireActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
@@ -36,7 +40,7 @@ public class HomeFragment extends Fragment {
                     .commit();
         });
 
-        Button club_finder = view.findViewById(R.id.club_finder);
+        CardView club_finder = view.findViewById(R.id.club_finder);
         club_finder.setOnClickListener(v -> {
             requireActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
 
@@ -49,19 +53,70 @@ public class HomeFragment extends Fragment {
                     .commit();
         });
 
-        Button predict_fide = view.findViewById(R.id.fide_prediction);
+        CardView predict_fide = view.findViewById(R.id.fide_prediction);
         predict_fide.setOnClickListener(v -> {
+                    FidePredictFragment fidePredictFragment = new FidePredictFragment();
 
+                    requireActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fidePredictFragment)
+                            .addToBackStack(null)
+                            .commit();
+                }
+        );
 
-            FidePredictFragment fidePredictFragment = new FidePredictFragment();
+        CardView puzzle = view.findViewById(R.id.puzzle);
+        puzzle.setOnClickListener(v -> {
             requireActivity().findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+
+            PuzzleFragment puzzleFragment = new PuzzleFragment();
+
             requireActivity().getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container, fidePredictFragment)
+                    .replace(R.id.fragment_container, puzzleFragment)
                     .addToBackStack(null)
                     .commit();
         });
+
+        CardView changeLang = view.findViewById(R.id.change_lang);
+
+        changeLang.setOnClickListener(this::showLanguageMenu);
+
         return view;
     }
-}
 
+    private void showLanguageMenu(View anchor) {
+        PopupMenu popup = new PopupMenu(requireContext(), anchor);
+        popup.getMenuInflater().inflate(R.menu.language, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.lang_en) {
+                setLocale("en");
+                return true;
+            } else if (itemId == R.id.lang_fr) {
+                setLocale("fr");
+                return true;
+            } else if (itemId == R.id.lang_vi) {
+                setLocale("vi");
+                return true;
+            }
+            return false;
+        });
+
+        popup.show();
+    }
+
+    private void setLocale(String langCode) {
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        requireContext().getResources().updateConfiguration(
+                config, requireContext().getResources().getDisplayMetrics()
+        );
+
+        // Optionally reload fragment or activity
+        requireActivity().recreate();
+    }
+}
--- app/src/main/java/com/ldlda/chesscom_stats/ui/puzzle/PuzzleFragment.java
+++ app/src/main/java/com/ldlda/chesscom_stats/ui/puzzle/PuzzleFragment.java
@@ -1,12 +1,13 @@
-package com.ldlda.chesscom_stats;
+package com.ldlda.chesscom_stats.ui.puzzle;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.ldlda.chesscom_stats.R;
 
 public class PuzzleFragment extends Fragment {
     @Override
