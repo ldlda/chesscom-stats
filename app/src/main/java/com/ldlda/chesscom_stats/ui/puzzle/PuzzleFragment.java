84bfd8205786af71c4bd1235c08677ec62a64d7c
BSFactor <2005nguyennamkhanh@gmail.com>
2025-10-14 23:31:11 +0700

    Puzzle fragment

--- app/src/main/java/com/ldlda/chesscom_stats/ui/puzzle/PuzzleFragment.java
+++ app/src/main/java/com/ldlda/chesscom_stats/ui/puzzle/PuzzleFragment.java
@@ -4,17 +4,68 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.button.MaterialButton;
 import com.ldlda.chesscom_stats.R;
+import com.ldlda.chesscom_stats.java_api.ApiClient;
+import com.ldlda.chesscom_stats.java_api.PlayerStatsData;
+import com.ldlda.chesscom_stats.java_api.Puzzle;
+import com.ldlda.chesscom_stats.java_api.PuzzleData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PuzzleFragment extends Fragment {
+    private ProgressBar progressBar;
+    private MaterialButton solveURI;
+    private ImageView puzzleView;
+    private TextView puzzleName;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_puzzle, container, false);
+        View view = inflater.inflate(R.layout.fragment_puzzle, container, false);
+        progressBar = view.findViewById(R.id.prog_bar);
+        solveURI = view.findViewById(R.id.puzzleURI);
+        puzzleView = view.findViewById(R.id.puzzleView);
+        puzzleName = view.findViewById(R.id.puzzle_name);
+
+        Puzzle puzzleApi = ApiClient.getClient().create(Puzzle.class);
+
+        Call<PuzzleData> statCall = puzzleApi.getDailyPuzzleData();
+
+        progressBar.setVisibility(View.VISIBLE);
+        statCall.enqueue(new Callback<PuzzleData>() {
+            @Override
+            public void onResponse(Call<PuzzleData> call, Response<PuzzleData> response) {
+                progressBar.setVisibility(View.GONE);
+                if (response.isSuccessful() && response.body() != null) {
+                    PuzzleData puzzle = response.body();
+                    puzzleName.setText(puzzle.title);
+                    Glide.with(requireContext())
+                            .load(puzzle.image)
+                            .into(puzzleView);
+                }else{
+                    Toast.makeText(requireContext(),"Error getting puzzle-Response code:"+ response.code(),Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PuzzleData> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(requireContext(),"Error getting puzzle: "+ t.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
     @Override
